57cb24521aa582830d0f898fda9e8936ddc6c8a9
HA <>
2022-01-15 14:08:26 +0800

    update

--- packages/hardhat/contracts/DistributionManager.sol
+++ packages/hardhat/contracts/DistributionManager.sol
@@ -6,7 +6,30 @@ import "./distributionTemplates/Lootbox.sol";
 import "hardhat/console.sol";
 
 contract DistributionManager {
+    address[] public campaigns;
+    mapping (address => address[]) public userToCampaign;
+    mapping (address => address) public campaignToUser;
 
+    uint public commonNFTAmount;
+
+    function launchCampaignCommonNFT(params) {
+        CommonNFT newCampaign = new CommonNFT{salt: keccak256(abi.encode(msg.sender, commonNFTAmount))}(
+            // params
+        );
+        campaigns.push(newCampaign);
+
+    }
+
+    function launchCampaignLootbox(params) {
+        Lootbox newCampaign = new Lootbox{salt: keccak256(abi.encode(msg.sender, lootboxAmount))}(
+            // params
+        );
+        campaigns.push(newCampaign);
+    }
+
+    function getCampaigns() public view returns (address[]) {
+        return campaigns;
+    }
 }
 
 
