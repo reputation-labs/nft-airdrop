c1b456a4e019d2475580b861ab12ffd1878da911
HA <>
2022-01-15 19:00:45 +0800

    update test for minter role

--- packages/hardhat/test/DistributionManager.js
+++ packages/hardhat/test/DistributionManager.js
@@ -41,5 +41,14 @@ describe("DistributionManager contract", function () {
     expect(userCampaigns.length).to.equal(1);
     expect(userCampaigns[0]).to.equal(campaigns[0]);
 
+    const campaignContract = await ethers.getContractAt(
+      "CommonNFT",
+      campaigns[0]
+    );
+
+    const minterRole = await campaignContract.MINTER_ROLE();
+    expect(minterRole).to.equal(
+      "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6"
+    );
   });
 });
