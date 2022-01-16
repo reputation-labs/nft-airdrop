6f40afeba8af261afa792ca762a21e9eba7169b4
HA <>
2022-01-17 00:45:03 +0800

    update title

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function Header() {
   return (
       <PageHeader
-        title="Rostra"
+        title="NFT Airdrop"
         subTitle=""
         style={{ cursor: "pointer" }}
       />
